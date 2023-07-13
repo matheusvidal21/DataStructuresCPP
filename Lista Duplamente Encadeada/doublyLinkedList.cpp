8f502975a56208e653df9dfe4a415eb34071e69d
Matheus Vidal <102569695+matheusvidal21@users.noreply.github.com>
2023-07-13 08:18:17 -0300

    Update doublyLinkedList.cpp

--- Lista Duplamente Encadeada/doublyLinkedList.cpp
+++ Lista Duplamente Encadeada/doublyLinkedList.cpp
@@ -222,6 +222,143 @@ class ListaDuplamenteEncadeada{
       head = tail = NULL;
     }
 
+    // Método para inserir um elemento em qualquer lugar da lista.
+    void insert(int index, T value) {
+      if (index < 0 || index >= getSize()) {
+        throw std::out_of_range("Index não encontrado.");
+      }
+    
+      No<T>* newNo = new No<T>(value);
+      No<T>* current = head;
+    
+      for (int i = 0; i < index; i++) {
+        current = current->next;
+      }
+    
+      newNo->next = current->next;
+      newNo->prev = current;
+      current->next->prev = newNo;
+      current->next = newNo;
+    }
+    
+    // Método para encontrar um elemento na lista.
+    T find(T value) {
+      No<T>* current = head;
+    
+      while (current != NULL) {
+        if (current->valor == value) {
+          return current->valor;
+        }
+        current = current->next;
+      }
+    
+      return NULL;
+    }
+    
+    // Método para classificar os elementos da lista.
+    void sort() {
+      if (head == NULL) {
+        return;
+      }
+    
+      No<T>* current = head;
+      No<T>* next = current->next;
+    
+      while (next != NULL) {
+        if (current->valor > next->valor) {
+          T temp = current->valor;
+          current->valor = next->valor;
+          next->valor = temp;
+        }
+    
+        current = next;
+        next = current->next;
+      }
+    }
+    
+    // Método para inverter os elementos da lista.
+    void reverse() {
+      if (head == NULL) {
+        return;
+      }
+    
+      No<T>* current = head;
+      No<T>* prev = NULL;
+      No<T>* next = current->next;
+    
+      while (next != NULL) {
+        current->next = prev;
+        prev = current;
+        current = next;
+        next = current->next;
+      }
+    
+      head = prev;
+    }
+    
+    // Método para clonar a lista.
+    ListaDuplamenteEncadeada<T> clone() {
+      ListaDuplamenteEncadeada<T> newList = ListaDuplamenteEncadeada<T>();
+    
+      No<T>* current = head;
+    
+      while (current != NULL) {
+        newList.pushBack(current->valor);
+        current = current->next;
+      }
+    
+      return newList;
+    }
+    
+    // Método para converter a lista em uma string.
+    std::string toString() {
+      std::string str;
+    
+      No<T>* current = head;
+    
+      while (current != NULL) {
+        str += std::to_string(current->valor) + " ";
+        current = current->next;
+      }
+    
+      return str;
+    }
+    
+    // Método para calcular o hashCode da lista.
+    std::size_t hashCode() {
+      std::size_t hash = 0;
+    
+      No<T>* current = head;
+    
+      while (current != NULL) {
+        hash = hash * 31 + current->valor;
+        current = current->next;
+      }
+    
+      return hash;
+    }
+    
+    // Método para comparar duas listas.
+    bool equals(ListaDuplamenteEncadeada<T> otherList) {
+      if (this->getSize() != otherList.getSize()) {
+        return false;
+      }
+    
+      No<T>* current = head;
+      No<T>* otherCurrent = otherList.head;
+    
+      while (current != NULL) {
+        if (current->valor != otherCurrent->valor) {
+          return false;
+        }
+    
+        current = current->next;
+        otherCurrent = otherCurrent->next;
+      }
+    
+      return true;
+    }
+
 };
 
 int main() {
